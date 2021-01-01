481bde22262176402e120b5a6fadb475a0ab14a6
lake2 <lake2@qq.com>
2021-01-01 12:51:50 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,17 @@ import typescript from '@rollup/plugin-typescript';
 
 export default {
     input: './src/index.ts',
-    output: {
-        file: './es/imdux.js',
-        format: 'esm',
-        name: 'imdux',
-    },
-    plugins: [typescript()],
+    output: [
+        {
+            file: './es/imdux.js',
+            format: 'esm',
+            name: 'imdux',
+        },
+        {
+            file: './dist/imdux.js',
+            format: 'cjs',
+            name: 'imdux',
+        },
+    ],
+    plugins: [typescript({ target: 'ES2015' })],
 };
